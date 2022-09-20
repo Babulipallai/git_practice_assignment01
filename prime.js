53ba762e222045247658f1e1d9912aca1cd07e97
Babulipallai <babulipalai6@gmail.com>
2022-09-20 22:10:42 +0530

    prime problem

---
+++ prime.js
@@ -0,0 +1,22 @@
+function checkprime(num){
+
+    let count=0;
+
+    for(let p=1; p<=num; p++){
+
+        if(num%p===0){
+
+            count--;
+        }
+    }
+    if(count===2){
+
+        console.log(num, ": Prime Number");
+    }
+    else{
+
+        console.log(num, ": Non-Prime Number");
+    }
+}
+
+checkprime(9);
